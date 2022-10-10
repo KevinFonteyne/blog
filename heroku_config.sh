8e24d84a17f012c880a9e0059b8a36fdedae99b5
Kevin Fonteyne <kfonteyne@outlook.com>
2022-10-10 18:02:34 -0400

    Added heroku_config.sh

--- heroku_config.sh
+++ heroku_config.sh
@@ -2,5 +2,5 @@
 heroku config:set SECRET_KEY=VHQW90JAHrljUaCPvEb286nU4-uHCBqa_xACkDnoRAI
 heroku config:set EMAIL_HOST=smtp.sendgrid.net
 heroku config:set EMAIL_HOST_USER=apikey
-heroku config:set EMAIL_HOST_PASSWORD=	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
+heroku config:set EMAIL_HOST_PASSWORD=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
 heroku config:set DEFAULT_FROM_EMAIL=kfonteyne@outlook.com
